a22abe523642b8ebbd3fd79d87eb1d2dfac9b893
Harsha <harsha.sam23@gmail.com>
2020-12-22 08:51:36 +0530

    Added 8.Lorem Ipsum Generator

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
+
+    const [num, setNum] = useState(0);
+    const [filterdData, setFilteredData] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFilteredData((data.slice(0, num)));
+    }
+
+    return (
+        <section className="section-center">
+            <h3>Tired of Boring Lorem Ipsum?</h3>
+            <form onSubmit={handleSubmit} className="lorem-form">
+                <label htmlFor="amount">
+                    Paragrahps:
+                </label>
+                <input
+                    id="amount"
+                    name="amount"
+                    type="number"
+                    value={num}
+                    onChange={(e) => setNum(e.target.value)}
+                />
+                <button className="btn" type="submit">Generate</button>
+            </form>
+            {
+                filterdData.map((para, index) => <p key={index} className="result">{para}</p>)
+            }
+        </section>
     )
 }
 
